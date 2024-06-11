2368b3fac70d704e7af5b6985fd77ec1e470cfb8
RafaelGBZ <rafael.fortegroleau.iut@gmail.com>
2024-06-11 16:22:54 +0200

    Add files via upload

--- sae23.sql
+++ sae23.sql
@@ -1,4 +1,5 @@
 -- Création de la base de données
+DROP DATABASE sae23;
 CREATE DATABASE sae23;
 
 -- Utilisation de la base de données
@@ -35,8 +36,8 @@ CREATE TABLE salle
 );
 
 -- Création de la table 'capteur'
-CREATE TABLE capteur
- (
+CREATE TABLE capteur 
+(
     id_capteur INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(30) NOT NULL UNIQUE,
     type_capteur VARCHAR(30) NOT NULL,
@@ -45,13 +46,13 @@ CREATE TABLE capteur
 );
 
 -- Création de la table 'mesure'
-CREATE TABLE mesure 
+CREATE TABLE mesure
 (
     id_mesure INT AUTO_INCREMENT PRIMARY KEY,
     date_mesure DATE NOT NULL,
-    horaire TIME NOT NULL,
-    valeur DECIMAL(6, 2) NOT NULL,
-    id_capteur INT
+	horaire TIME NOT NULL,
+	valeur DECIMAL(6, 2)NOT NULL,
+	id_capteur INT
 );
 
 -- Création de la table 'administration'
@@ -97,61 +98,52 @@ CREATE TABLE composer
 ALTER TABLE generer
 ADD CONSTRAINT fk_generer_capteur
 FOREIGN KEY (id_capteur)
-REFERENCES capteur(id_capteur)
-ON DELETE CASCADE;
+REFERENCES capteur(id_capteur);
 
 -- Contrainte de clé étrangère pour la table 'generer' reliant 'id_mesure' à 'mesure(id_mesure)'
 ALTER TABLE generer
 ADD CONSTRAINT fk_generer_mesure
 FOREIGN KEY (id_mesure)
-REFERENCES mesure(id_mesure)
-ON DELETE CASCADE;
+REFERENCES mesure(id_mesure);
 
 -- Contrainte de clé étrangère pour la table 'installer' reliant 'id_salle' à 'salle(id_capteur)'
 ALTER TABLE installer
 ADD CONSTRAINT fk_installer_salle
 FOREIGN KEY (id_salle)
-REFERENCES salle(id_salle)
-ON DELETE CASCADE;
+REFERENCES salle(id_salle);
 
 -- Contrainte de clé étrangère pour la table 'installer' reliant 'id_capteur' à 'capteur(id_capteur)'
 ALTER TABLE installer
 ADD CONSTRAINT fk_installer_capteur
 FOREIGN KEY (id_capteur)
-REFERENCES capteur(id_capteur)
-ON DELETE CASCADE;
+REFERENCES capteur(id_capteur);
 
 -- Contrainte de clé étrangère pour la table 'composer' reliant 'id_batiment' à 'batiment(id_batiment)'
 ALTER TABLE composer
 ADD CONSTRAINT fk_composer_batiment
 FOREIGN KEY (id_batiment)
-REFERENCES batiment(id_batiment)
-ON DELETE CASCADE;
+REFERENCES batiment(id_batiment);
 
 -- Contrainte de clé étrangère pour la table 'composer' reliant 'id_salle' à 'salle(id_salle)'
 ALTER TABLE composer
 ADD CONSTRAINT fk_composer_salle
 FOREIGN KEY (id_salle)
-REFERENCES salle(id_salle)
-ON DELETE CASCADE;
+REFERENCES salle(id_salle);
 
 -- Contrainte de clé étrangère pour la table 'salle' reliant 'id_batiment' à 'composer(id_capteur)'
 ALTER TABLE salle
 ADD CONSTRAINT fk_batiment
 FOREIGN KEY (id_batiment)
-REFERENCES composer(id_batiment)
-ON DELETE CASCADE;
+REFERENCES composer(id_batiment);
 
 -- Contrainte de clé étrangère pour la table 'capteur' reliant 'id_salle' à 'installer(id_salle)'
 ALTER TABLE capteur
 ADD CONSTRAINT fk_salle
 FOREIGN KEY (id_salle)
-REFERENCES installer(id_salle)
-ON DELETE CASCADE;
+REFERENCES installer(id_salle);
 
 -- Contrainte de clé étrangère pour la table 'mesure' reliant 'id_capteur' à 'generer(id_capteur)'
 ALTER TABLE mesure
 ADD CONSTRAINT fk_capteur
 FOREIGN KEY (id_capteur)
-REFERENCES generer(id_capteur)
-ON DELETE CASCADE;
+REFERENCES generer(id_capteur);
