4f344440e8a4d58b87b058e2eea1375b4ac631f7
Faresrt <133324226+Faresrt@users.noreply.github.com>
2024-06-13 16:47:12 +0200

    Add files via upload

--- sae23.sql
+++ sae23.sql
@@ -1,45 +1,35 @@
 -- Création de la base de données
-DROP DATABASE sae23;
+DROP DATABASE IF EXISTS sae23;
 CREATE DATABASE sae23;
 
 -- Utilisation de la base de données
 USE sae23;
-
--- Ordre de suppression des tables
-DROP TABLE IF EXISTS generer;
-DROP TABLE IF EXISTS installer;
-DROP TABLE IF EXISTS composer;
-DROP TABLE IF EXISTS mesure;
-DROP TABLE IF EXISTS capteur;
-DROP TABLE IF EXISTS salle;
-DROP TABLE IF EXISTS batiment;
-DROP TABLE IF EXISTS administration;
 
 -- Création de la table 'batiment'
 CREATE TABLE batiment 
 (
     id_batiment INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(30) NOT NULL,
-	id_gestionnaire INT,
+    id_gestionnaire INT,
     login VARCHAR(30) NOT NULL UNIQUE,
     mot_de_passe VARCHAR(50) NOT NULL
 );
 
 -- Création de la table 'salle'
 CREATE TABLE salle
- (
+(
     id_salle INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(30) NOT NULL UNIQUE,
     type_salle VARCHAR(30) NOT NULL,
     capacite_accueil INT NOT NULL,
-	id_batiment INT
+    id_batiment INT
 );
 
 -- Création de la table 'capteur'
 CREATE TABLE capteur 
 (
     id_capteur INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(30) NOT NULL UNIQUE,
+    nom_capteur VARCHAR(30) NOT NULL UNIQUE,
     type_capteur VARCHAR(30) NOT NULL,
     unite VARCHAR(20) NOT NULL,
     id_salle INT
@@ -50,9 +40,9 @@ CREATE TABLE mesure
 (
     id_mesure INT AUTO_INCREMENT PRIMARY KEY,
     date_mesure DATE NOT NULL,
-	horaire TIME NOT NULL,
-	valeur DECIMAL(6, 2)NOT NULL,
-	id_capteur INT
+    horaire TIME NOT NULL,
+    valeur DECIMAL(6, 2) NOT NULL,
+    nom_capteur VARCHAR(30) NOT NULL
 );
 
 -- Création de la table 'administration'
@@ -63,87 +53,74 @@ CREATE TABLE administration
     mot_de_passe VARCHAR(50) NOT NULL
 );
 
--- Création de la table 'generer'
-CREATE TABLE generer  
+-- Création de la table 'composer'
+CREATE TABLE composer
 (
-    id_capteur INT,
-	id_mesure INT,
-	PRIMARY KEY(id_capteur, id_mesure)
-	
+  id_batiment int(11) NOT NULL,
+  id_salle int(11) NOT NULL,
+  PRIMARY KEY (id_batiment, id_salle)
 );
 
 -- Création de la table 'installer'
 CREATE TABLE installer
 (
-    id_salle INT,
-	id_capteur INT,
-	PRIMARY KEY(id_salle, id_capteur)
-	
+  id_salle int(11) NOT NULL,
+  id_capteur int(11) NOT NULL,
+  PRIMARY KEY (id_salle, id_capteur)
 );
 
--- Création de la table 'composer'
-CREATE TABLE composer
+-- Création de la table 'generer'
+CREATE TABLE generer
 (
-    id_batiment INT,
-	id_salle INT,
-	PRIMARY KEY(id_batiment, id_salle)
-	
+  id_capteur int(11) NOT NULL,
+  id_mesure int(11) NOT NULL,
+  PRIMARY KEY (id_capteur, id_mesure)
 );
 
-
--- Ajout des contraintes de clé étrangère
-
-
--- Contrainte de clé étrangère pour la table 'generer' reliant 'id_capteur' à 'capteur(id_capteur)'
-ALTER TABLE generer
-ADD CONSTRAINT fk_generer_capteur
-FOREIGN KEY (id_capteur)
-REFERENCES capteur(id_capteur);
-
--- Contrainte de clé étrangère pour la table 'generer' reliant 'id_mesure' à 'mesure(id_mesure)'
-ALTER TABLE generer
-ADD CONSTRAINT fk_generer_mesure
-FOREIGN KEY (id_mesure)
-REFERENCES mesure(id_mesure);
-
--- Contrainte de clé étrangère pour la table 'installer' reliant 'id_salle' à 'salle(id_capteur)'
-ALTER TABLE installer
-ADD CONSTRAINT fk_installer_salle
-FOREIGN KEY (id_salle)
-REFERENCES salle(id_salle);
-
--- Contrainte de clé étrangère pour la table 'installer' reliant 'id_capteur' à 'capteur(id_capteur)'
-ALTER TABLE installer
-ADD CONSTRAINT fk_installer_capteur
-FOREIGN KEY (id_capteur)
-REFERENCES capteur(id_capteur);
-
--- Contrainte de clé étrangère pour la table 'composer' reliant 'id_batiment' à 'batiment(id_batiment)'
-ALTER TABLE composer
-ADD CONSTRAINT fk_composer_batiment
-FOREIGN KEY (id_batiment)
-REFERENCES batiment(id_batiment);
-
--- Contrainte de clé étrangère pour la table 'composer' reliant 'id_salle' à 'salle(id_salle)'
-ALTER TABLE composer
-ADD CONSTRAINT fk_composer_salle
-FOREIGN KEY (id_salle)
-REFERENCES salle(id_salle);
-
--- Contrainte de clé étrangère pour la table 'salle' reliant 'id_batiment' à 'composer(id_capteur)'
+-- Ajouter la contrainte de clé étrangère pour la table 'salle'
 ALTER TABLE salle
-ADD CONSTRAINT fk_batiment
-FOREIGN KEY (id_batiment)
-REFERENCES composer(id_batiment);
+ADD CONSTRAINT FK_salle_batiment
+FOREIGN KEY (id_batiment) REFERENCES batiment(id_batiment);
 
--- Contrainte de clé étrangère pour la table 'capteur' reliant 'id_salle' à 'installer(id_salle)'
+-- Ajouter la contrainte de clé étrangère pour la table 'capteur'
 ALTER TABLE capteur
-ADD CONSTRAINT fk_salle
-FOREIGN KEY (id_salle)
-REFERENCES installer(id_salle);
+ADD CONSTRAINT FK_capteur_salle
+FOREIGN KEY (id_salle) REFERENCES salle(id_salle);
 
--- Contrainte de clé étrangère pour la table 'mesure' reliant 'id_capteur' à 'generer(id_capteur)'
+-- Ajouter la contrainte de clé étrangère pour la table 'mesure'
 ALTER TABLE mesure
-ADD CONSTRAINT fk_capteur
-FOREIGN KEY (id_capteur)
-REFERENCES generer(id_capteur);
+ADD CONSTRAINT FK_mesure_capteur
+FOREIGN KEY (nom_capteur) REFERENCES capteur(nom_capteur);
+
+-- Ajouter la contrainte de clé étrangère pour la table 'composer'
+ALTER TABLE composer
+ADD CONSTRAINT FK_composer_batiment
+FOREIGN KEY (id_batiment) REFERENCES batiment(id_batiment);
+
+-- Ajouter la contrainte de clé étrangère pour la table 'composer'
+ALTER TABLE composer
+ADD CONSTRAINT FK_composer_salle
+FOREIGN KEY (id_salle) REFERENCES salle(id_salle);
+
+-- Ajouter la contrainte de clé étrangère pour la table 'installer'
+ALTER TABLE installer
+ADD CONSTRAINT FK_installer_salle
+FOREIGN KEY (id_salle) REFERENCES salle(id_salle);
+
+-- Ajouter la contrainte de clé étrangère pour la table 'installer'
+ALTER TABLE installer
+ADD CONSTRAINT FK_installer_capteur
+FOREIGN KEY (id_capteur) REFERENCES capteur(id_capteur);
+
+-- Ajouter la contrainte de clé étrangère pour la table 'generer'
+ALTER TABLE generer
+ADD CONSTRAINT FK_generer_capteur
+FOREIGN KEY (id_capteur) REFERENCES capteur(id_capteur);
+
+-- Ajouter la contrainte de clé étrangère pour la table 'generer'
+ALTER TABLE generer
+ADD CONSTRAINT FK_generer_mesure
+FOREIGN KEY (id_mesure) REFERENCES mesure(id_mesure);
+
+
+
